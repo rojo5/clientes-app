861033817592f61a6b2651e618cb0339d1335a87
rojo5 <luis21197@gmail.com>
2019-04-08 20:52:15 +0200

    Implementando componente Cliente

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import { Cliente} from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Luis', apellido: ' Lorenzo', email: 'luis@luis.com', createAt: '2019-04-01'},
+    {id: 2, nombre: 'Selva', apellido: 'Blanco', email: 'selva@selva.com', createAt: '2019-04-02'},
+    {id: 3, nombre: 'Maria', apellido: 'Lopez', email: 'maria@maria.com', createAt: '2019-04-03'},
+    {id: 4, nombre: 'Mario', apellido: 'Lorenzo', email: 'mario@mario.com', createAt: '2019-04-04'},
+    {id: 5, nombre: 'Guillermo', apellido: 'Barajas', email: 'guille@guille.com', createAt: '2019-04-05'},
+    {id: 6, nombre: 'Paula', apellido: 'Blanco', email: 'paula@paula.com', createAt: '2019-04-06'},
+    {id: 7, nombre: 'Rosa', apellido: 'Cuadrado', email: 'rosa@rosa.com', createAt: '2019-04-07'},
+    {id: 8, nombre: 'Cristina', apellido: 'Rey', email: 'cristina@cristina.com', createAt: '2019-04-08'},
+    {id: 9, nombre: 'Daniel', apellido: 'Alcon', email: 'daniel@daniel.com', createAt: '2019-04-09'},
+    {id: 10, nombre: 'Juan Jose', apellido: 'Lorenzo', email: 'juanjo@juanjo', createAt: '2019-04-10'},
+    {id: 11, nombre: 'Unai', apellido: 'Alba', email: 'unai@unai', createAt: '2019-04-11'}
+      ];
